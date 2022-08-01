58dec218643822e2a16a6b771e861b191d75524e
Nguyen Hoang Viet <vietvuive579@gmail.com>
2022-08-01 12:02:06 +0700

    first commit

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage = "Display Order for category must be greater than 0")]
+        public int DisplayOrder { get; set; }
+    }
+}
